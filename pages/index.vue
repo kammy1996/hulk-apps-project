22ddcaefc6f91d71666eaf3f25cb9dae761eb32c
kammy1996 <Kam_dev@786>
2022-06-07 19:17:54 +0530

    added scss and default css helpers

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <p>this is a project page</p>
-    <b-container>
-      <b-row>
-        <b-col>5</b-col>
-        <b-col>5</b-col>
-        <b-col>5</b-col>
-        <b-col>5</b-col>
-      </b-row>
-    </b-container>
+    <div class="height-50"></div>
+      <div class="heading text-center">
+        <h2 >Featured Collection</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s,
+        </p>
+      </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style lang="scss" scoped>
