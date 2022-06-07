1fecbed86f5a711860db188746597078588da1c9
kammy1996 <Kam_dev@786>
2022-06-07 19:02:26 +0530

    first commit in nuxt

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>this is a project page</p>
+    <b-container>
+      <b-row>
+        <b-col>5</b-col>
+        <b-col>5</b-col>
+        <b-col>5</b-col>
+        <b-col>5</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
